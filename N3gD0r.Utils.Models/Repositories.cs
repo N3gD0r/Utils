96959e29c3cb8cef105f92b95d3646bb51cc4b6c
N3gD0r <70918021+N3gD0r@users.noreply.github.com>
2025-12-25 17:07:00 -0600

    Tweaked Repository Update and Read abstractions
    Removed unnecessary parameter id when updating and added a read with
    only an id.

--- N3gD0r.Utils.Models/Repositories.cs
+++ N3gD0r.Utils.Models/Repositories.cs
@@ -6,13 +6,14 @@ public interface IWriteRepository<TEntity, TKey>
     where TEntity : IEntityBase<TKey>
 {
     Task CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
-    Task UpdateAsync(TKey id, TEntity entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
     Task DeleteAsync(TKey id, CancellationToken cancellationToken);
 }
 
 public interface IReadRepository<TEntity, TKey>
     where TEntity : IEntityBase<TKey>
 {
+    Task<TEntity> ReadByIdAsync(TKey id, CancellationToken cancellationToken = default);
     Task<IEnumerable<TEntity>> ReadAllAsync(CancellationToken cancellationToken = default);
     Task<IEnumerable<TEntity>> ReadByAsync(
         Expression<Func<TEntity, bool>> expression,
